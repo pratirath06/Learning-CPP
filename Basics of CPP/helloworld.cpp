4f20a16ad98afa81a45a6236096916366298809f
Pratirath Gupta <pratirathgupta51@gmail.com>
2024-07-16 22:19:09 +0530

    Create helloworld.cpp

---
+++ Basics of CPP/helloworld.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;//We are declaring that we are using standard namespace other way to do it is by writing std::cout<<"Hello World"<<endl;
+int main(){
+    //Print Hello World.
+    cout<<"Hello World\n";
+    return 0;
+}
