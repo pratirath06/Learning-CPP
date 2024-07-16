231eb950b73c38dbfb9a1aa73fafa4d8906045f0
Pratirath Gupta <pratirathgupta51@gmail.com>
2024-07-16 23:44:06 +0530

    Create fibbionacci.cpp

---
+++ Basics of CPP/fibbionacci.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //This code is for basic implementation of for loop
+    int num;
+    cin>>num;
+    for(int i=1;i<11;i++)//As seen there are three arguments inside for loop and they are (initialization;condition;update)
+    {
+        cout<<num<<" X "<<i<<" = "<<num*i<<endl;
+    }
+    return 0;
+}
