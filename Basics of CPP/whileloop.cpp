f889247680ac282f1166e6277597707991e956dd
Pratirath Gupta <pratirathgupta51@gmail.com>
2024-07-17 22:21:37 +0530

    Create whileloop.cpp

---
+++ Basics of CPP/whileloop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //This code is for basic implementation of while loop, it will continue loop until we give positive input
+    int num;
+    cin>>num;
+    while(num>0){
+        cout<<"Positive";
+        cin>>num;
+    }
+    return 0;
+}
