bab23fb225f512fc9b1131b5d2b3a5b2c9e8316d
Pratirath Gupta <pratirathgupta51@gmail.com>
2024-07-22 21:59:18 +0530

    Create arrays.cpp

---
+++ Basics of CPP/arrays.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //This code is includes input and output of arrays.
+    int num[10],n=10;
+    for(int i=0;i<n;i++){
+        cin>>num[i];
+    }
+    for(int i=0;i<n;i++){
+        cout<<num[i];
+    }
+}
